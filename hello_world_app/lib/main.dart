c11f1e0327a281f6e01f647ce51d36bfcea20078
James Hernandez <jhon.james.1995@gmail.com>
2025-06-25 20:44:44 -0500

    Borrar app normal para agregar cosas cambios desde 0

--- hello_world_app/lib/main.dart
+++ hello_world_app/lib/main.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(child: Text('Hola mundo'))),
+    );
+  }
+}
